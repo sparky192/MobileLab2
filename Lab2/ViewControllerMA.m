9550688a8b1e3d87bd3a00ccf43b773e0e35f121
sparky192 <mphadate@smu.edu>
2017-09-21 20:41:52 -0500

    Mod A working yolo

--- Lab2/ViewControllerMA.m
+++ Lab2/ViewControllerMA.m
@@ -13,25 +13,34 @@
 #import "FFTHelper.h"
 #import "PeakFinder.h"
 
-#define DISPLAY_SIZE 2048
-#define BUFFER_SIZE 2048*2
-#define PADDED_SIZE 14700*2
+#define DISPLAY_SIZE 8000
+#define BUFFER_SIZE 16384
+#define READ_SIZE 1024*4
+
 
 @interface ViewControllerMA ()
 @property (strong, nonatomic) Novocaine *audioManager;
 @property (weak, nonatomic) IBOutlet UILabel *FreqLabel;
 @property (weak, nonatomic) IBOutlet UILabel *Freq2;
 @property (strong, nonatomic) CircularBuffer *buffer;
+@property (weak, nonatomic) IBOutlet UISlider *ampSlider;
 @property (strong, nonatomic) SMUGraphHelper *graphHelper;
 @property (strong, nonatomic) FFTHelper *fftHelper;
 @property(strong, nonatomic)PeakFinder *finder;
 @property float freq;
+@property float mag;
+@property float maxMag;
+@property BOOL lock;
 @end
 
 
 
 @implementation ViewControllerMA
 @synthesize freq;
+@synthesize mag;
+@synthesize lock;
+@synthesize maxMag;
+
 
 #pragma mark Lazy Instantiation
 -(Novocaine*)audioManager{
@@ -43,7 +52,7 @@
 
 -(CircularBuffer*)buffer{
     if(!_buffer){
-        _buffer = [[CircularBuffer alloc]initWithNumChannels:1 andBufferSize:PADDED_SIZE];
+        _buffer = [[CircularBuffer alloc]initWithNumChannels:1 andBufferSize:BUFFER_SIZE];
     }
     return _buffer;
 }
@@ -54,14 +63,14 @@
                                         preferredFramesPerSecond:15
                                                        numGraphs:2
                                                        plotStyle:PlotStyleSeparated
-                                               maxPointsPerGraph:PADDED_SIZE];
+                                               maxPointsPerGraph:DISPLAY_SIZE];
     }
     return _graphHelper;
 }
 
 -(FFTHelper*)fftHelper{
     if(!_fftHelper){
-        _fftHelper = [[FFTHelper alloc]initWithFFTSize:PADDED_SIZE];
+        _fftHelper = [[FFTHelper alloc]initWithFFTSize:BUFFER_SIZE];
     }
     
     return _fftHelper;
@@ -70,7 +79,9 @@
 
 -(PeakFinder*)finder{
     if (!_finder) {
-        _finder = [[PeakFinder alloc] initWithFrequencyResolution:3.0];
+        float res = 44100.0/BUFFER_SIZE;
+        _finder = [[PeakFinder alloc] initWithFrequencyResolution:res];
+        NSLog(@"Freq Res : %f",res);
     }
     return _finder;
 }
@@ -80,7 +91,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    CGFloat angel = -(M_PI/2);
+    self.ampSlider.transform = CGAffineTransformMakeRotation(angel);
+    lock = NO;
     
     [self.graphHelper setScreenBoundsBottomHalf];
     
@@ -98,33 +111,33 @@
     // just plot the audio stream
     
     // get audio stream data
-    float* arrayData = malloc(sizeof(float)*PADDED_SIZE);
-    float* fftMagnitude = malloc(sizeof(float)*PADDED_SIZE/2);
-    float* arrayDataPadded = malloc(sizeof(float)*PADDED_SIZE);
+    float* arrayData = malloc(sizeof(float)*BUFFER_SIZE);
+    float* fftMagnitude = malloc(sizeof(float)*BUFFER_SIZE/2);
+//    float* arrayDataPadded = malloc(sizeof(float)*PADDED_SIZE);
     
-    [self.buffer fetchFreshData:arrayDataPadded withNumSamples:PADDED_SIZE];
+    [self.buffer fetchFreshData:arrayData withNumSamples:READ_SIZE];
     
-//    for (int i=0; i < PADDED_SIZE; i++){
+    for (int i=READ_SIZE; i < BUFFER_SIZE; i++){
 //        if (i < BUFFER_SIZE)
 //            arrayDataPadded[i]  = arrayData[i];
 //        else{
-//            arrayDataPadded[i] = 0.00001;
+            arrayData[i] = 0.0;
 //        }
-//    }
+    }
     
     
     //send off for graphing
-    [self.graphHelper setGraphData:arrayDataPadded
+    [self.graphHelper setGraphData:arrayData
                     withDataLength:DISPLAY_SIZE
                      forGraphIndex:0];
     
     // take forward FFT
-    [self.fftHelper performForwardFFTWithData:arrayDataPadded
+    [self.fftHelper performForwardFFTWithData:arrayData
                    andCopydBMagnitudeToBuffer:fftMagnitude];
     
     // graph the FFT Data
     [self.graphHelper setGraphData:fftMagnitude
-                    withDataLength:DISPLAY_SIZE/2
+                    withDataLength:DISPLAY_SIZE
                      forGraphIndex:1
                  withNormalization:64.0
                      withZeroValue:-60];
@@ -132,22 +145,47 @@
     
     
     
-    NSArray *peaks = [self.finder getFundamentalPeaksFromBuffer: fftMagnitude withLength:PADDED_SIZE/2 usingWindowSize:10 andPeakMagnitudeMinimum:-10 aboveFrequency:500] ;
+    NSArray *peaks = [self.finder getFundamentalPeaksFromBuffer: fftMagnitude
+                                                     withLength:BUFFER_SIZE/2
+                                                usingWindowSize:20
+                                        andPeakMagnitudeMinimum:mag
+                                                 aboveFrequency:150] ;
     Peak *p1 = [peaks objectAtIndex:0];
     
     
     //float freq = [[peaks objectAtIndex:0] ge];
-   self.FreqLabel.text = [NSString stringWithFormat:@"F1: %f", p1.frequency];
-    NSLog(@" Freq : %@",[peaks objectAtIndex:1]);
-    if(![peaks objectAtIndex:1]){
-        Peak *p2 = [peaks objectAtIndex:1];
-        self.Freq2.text = [NSString stringWithFormat:@"F2: %f", p2.frequency];
+  //  float x = p1.frequency*0.90909;
+    
+    if(!lock){
+       self.FreqLabel.text = [NSString stringWithFormat:@"F1: %f" , p1.frequency];
+
+        if([peaks count] >1){
+            Peak *p2 = [peaks objectAtIndex:1];
+            self.Freq2.text = [NSString stringWithFormat:@"F2: %f", p2.frequency];
+        }else{
+            self.Freq2.text = [NSString stringWithFormat:@"F2: 0.000000" ];
+        }
+    }else{
+        
+        if( p1.magnitude > mag){
+            self.FreqLabel.text = [NSString stringWithFormat:@"F1: %f" , p1.frequency];
+            if([peaks count] >1){
+                Peak *p2 = [peaks objectAtIndex:1];
+                self.Freq2.text = [NSString stringWithFormat:@"F2: %f", p2.frequency];
+            }else{
+                self.Freq2.text = [NSString stringWithFormat:@"F2: 0.000000" ];
+            }
+        }
         
     }
+    
+    
+    
+    
     [self.graphHelper update]; // update the graph
     free(arrayData);
     free(fftMagnitude);
-    free(arrayDataPadded);
+    //free(arrayDataPadded);
     
 }
 
@@ -156,5 +194,19 @@
     [self.graphHelper draw]; // draw the graph
 }
 
+- (IBAction)setAmpThrashold:(UISlider *)sender {
+    mag = self.ampSlider.value;
+    NSLog(@"Amp Threshold Set to : %f", mag);
+}
+- (IBAction)toggleFreqLock:(UIButton *)sender {
+    if(lock){
+        lock = NO;
+    }
+    else{
+        lock = YES;
+        mag = 0;
+    }
+    NSLog(@"Toggle : %d",lock);
+}
 
 @end
